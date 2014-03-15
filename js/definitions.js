ee0ff5911a3c19b2b567488ff8aad638e6b09f69
Jan Schulte <hello@unexpected-co.de>
2014-03-15 21:23:33 +0100

    Adapted readme

--- js/definitions.js
+++ js/definitions.js
@@ -10,8 +10,7 @@ function def() {
     "256": 19,
     "512": 23,
     "1024": 29,
-    "2048": 31,
-    "4096": 37
+    "2048": 31
   };
   this.map = function (pow) {
     return this.powToPrime[pow.toString()];
